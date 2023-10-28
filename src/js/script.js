78a4c6395ce9e597de232eed51e50cfba8eff82b
crazyapple108 <crazyapple108@gmail.com>
2023-10-28 22:19:26 +0900

    提出前確認終了

--- src/js/script.js
+++ src/js/script.js
@@ -7,17 +7,15 @@ jQuery(function ($) { // この中であればWordpressでも「$」が使用可
     clickable: true,
     loop: true,
     loopAdditionalSlides: 1,
-    autoplay: {
-      delay: 3000
-    }
+    // autoplay: {
+    //   delay: 3000
+    // }
   });
 
   const mySwiper = new Swiper('.swiper-campaign', {
     slidesPerView: '1.2',
     spaceBetween: 24,
     loop: true,
-    // loopAdditionalSlides: 1,
-    // freeMode: true, // 前後のスライドで止まらずにスライド
     grabCursor: true, // カーソルを置いたときに指のカーソルを表示
     navigation: {
       nextEl: '.swiper-campaign-button-prev',
@@ -63,79 +61,6 @@ jQuery(function ($) { // この中であればWordpressでも「$」が使用可
 	$(window).on('load', function(){
 		fadeAnime();/* アニメーション用の関数を呼ぶ*/
 	});// ここまで画面が読み込まれたらすぐに動かしたい場合の記述
-
-  // /******************************************
-  // init
-  // ******************************************/
-  // var startPos = 0,
-  //   winScrollTop = 0;
-  // /******************************************
-  // window scroll
-  // ******************************************/
-  // // $(window).on('scroll', function () {
-  // //   winScrollTop = $(this).scrollTop();
-  // //   if (winScrollTop >= startPos) {
-  // //     if (winScrollTop >= 200) {
-  // //       $('.header').addClass('hide');
-  // //     }
-  // //   } else {
-  // //     $('.header').removeClass('hide');
-  // //   }
-  // //   startPos = winScrollTop;
-  // // });
-  // var trigger = new ScrollTrigger["default"]();
-  // trigger.add('[data-trigger]');
-
-  // var isAnimated = false;
-
-  // function delayScrollAnime() {
-  //   var time = 0.2; //遅延時間を増やす秒数の値
-  //   var value = time;
-  //   $('.delay-scroll').each(function () {
-  //     var parent = this; //親要素を取得
-  //     var elemPos = $(this).offset().top; //要素の位置まで来たら
-  //     var scroll = $(window).scrollTop(); //スクロール値を取得
-  //     var windowHeight = $(window).height(); //画面の高さを取得
-  //     var childs = $(this).children(); //子要素を取得
-
-  //     // アニメーションが既に実行された場合はスキップ
-  //   if (isAnimated) {
-  //     return;
-  //   }
-  //     if (scroll >= elemPos - windowHeight && !$(parent).hasClass("play")) {
-  //       //指定領域内にスクロールが入ったらまた親要素にクラスplayがなければ
-  //       $(childs).each(function () {
-  //         if (!$(this).hasClass("js-fade-up")) {
-  //           //アニメーションのクラス名が指定されているかどうかをチェック
-  //           $(parent).addClass("play"); //親要素にクラス名playを追加
-  //           $(this).css("animation-delay", value + "s"); //アニメーション遅延のCSS animation-delayを追加し
-  //           $(this).addClass("js-fade-up"); //アニメーションのクラス名を追加
-  //           value = value + time; //delay時間を増加させる
-  //           //全ての処理を終わったらplayを外す
-  //           var index = $(childs).index(this);
-  //           if (childs.length - 1 == index) {
-  //             $(parent).removeClass("play");
-  //           }
-            
-  //         }
-  //       });
-  //       isAnimated = true;
-  //     } else {
-  //       $(childs).removeClass("js-fade-up"); //アニメーションのクラス名を削除
-  //       value = time; //delay初期値の数値に戻す
-  //     }
-  //   });
-  // }
-
-  // // 画面をスクロールをしたら動かしたい場合の記述
-  // $(window).scroll(function () {
-  //   delayScrollAnime(); /* アニメーション用の関数を呼ぶ*/
-  // }); // ここまで画面をスクロールをしたら動かしたい場合の記述
-
-  // 画面が読み込まれたらすぐに動かしたい場合の記述
-  // $(window).on('load', function () {
-  //   delayScrollAnime(); /* アニメーション用の関数を呼ぶ*/
-  // }); // ここまで画面が読み込まれたらすぐに動かしたい場合の記述
 
    //フッター手前で止まるボタン
 $(function() {
