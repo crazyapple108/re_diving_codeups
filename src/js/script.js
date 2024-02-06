c854b11ecc4c0c62a93dc32dd7f7f94acb102579
crazyapple108 <crazyapple108@gmail.com>
2024-02-07 00:52:36 +0900

    blog-archive spピクパ完了

--- src/js/script.js
+++ src/js/script.js
@@ -142,4 +142,31 @@ $('.js-tab-menu').on('click', function () {
   $('#' + number).addClass('is-active');
 });
 
+let accordionDetails = '.js-accordion';
+let accordionSummary = '.js-accordion__title';
+let accordionContent = '.js-accordion__content';
+let speed = 200;
+
+$(accordionSummary).each(function (){
+    $(this).on("click",function (event){
+        // デフォルトの挙動を無効化
+        event.preventDefault();
+        if($(this).parent($(accordionDetails)).attr("open")){
+            // アコーディオンを閉じるときの処理
+            $(this).nextAll($(accordionContent)).slideUp(speed,function (){
+                // アニメーションの完了後にopen属性を取り除く
+                $(this).parent($(accordionDetails)).removeAttr("open");
+                // display:none;を消して、ページ内検索にヒットするようにする
+                $(this).show();
+            });
+        }else{
+            // アコーディオンを開くときの処理
+            // open属性を付ける
+            $(this).parent($(accordionDetails)).attr("open","true");
+            // いったんdisplay:none;してからslideDownで開く
+            $(this).nextAll($(accordionContent)).hide().slideDown(speed);
+        }
+    })
+})
+
 });
