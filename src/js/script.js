13f95047f5e90487688c7c46161be5c0d41986b8
crazyapple108 <crazyapple108@gmail.com>
2024-02-12 01:01:40 +0900

    faq 完了

--- src/js/script.js
+++ src/js/script.js
@@ -169,4 +169,9 @@ $(accordionSummary).each(function (){
     })
 })
 
+$('.js-faq-question').on('click', function () {
+  $(this).next().slideToggle();
+  $(this).toggleClass('is-open');
+});
+
 });
